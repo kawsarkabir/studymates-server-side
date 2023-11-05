bc23c9d8c35601f54cbf6935b85e36d51858d9b2
kawsarkabir <devkawsarkabir@gmail.com>
2023-11-05 23:14:50 +0600

    view assingment page dynamicaly done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -40,6 +40,14 @@ async function run() {
     app.get("/assingments", async (req, res) => {
       res.send(await assingmentCollection.find().toArray());
     });
+    // get single items
+    app.get("/assingments/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        res.send(await assingmentCollection.findOne(query));
+      });
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
